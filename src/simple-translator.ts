7f53ed5233146919375fdce0f3671b42866c1256
William Okafor <wilpat456@gmail.com>
2025-05-11 19:31:57 +0100

    chore: use inquirer for language selection

--- src/simple-translator.ts
+++ src/simple-translator.ts
@@ -2,6 +2,8 @@ require('dotenv').config()
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import * as readline from 'readline';
+import inquirer from 'inquirer';
+
 const model = new ChatOpenAI({ model: "gpt-4" });
 
 const rl = readline.createInterface({
@@ -16,24 +18,46 @@ const promptTemplate = ChatPromptTemplate.fromMessages([
     ["user", "{textToTranslate}"],
 ]);
 
+const languages = [
+    "Italian",
+    "Spanish",
+    "French",
+    "German",
+    "Dutch",
+    "Swedish",
+    "Russian",
+    "Polish",
+    "Czech"
+  ];
+
 const runTranlator = async () => {
     rl.question("Enter text to translate: ", async (textToTranslate) => {
         if (!textToTranslate) {
             console.log("You need to enter a text to translate. Please try again.");
             runTranlator();
+            return
         }
-        rl.question("Enter destination language (Italian): ", async (destinationLanguage) => {
-            const prompt = await promptTemplate.invoke({ textToTranslate, destinationLanguage: defaultLanguage ?? destinationLanguage });
+        const { destinationLanguage } = await inquirer.prompt([
+            {
+              type: 'list',
+              name: 'destinationLanguage',
+              message: 'Select the destination language:',
+              choices: languages,
+              loop: false,
+            }
+          ]);
+        const prompt = await promptTemplate.invoke({ textToTranslate, destinationLanguage: defaultLanguage ?? destinationLanguage });
 
-            const res = await model.invoke(prompt);
-            console.log({ translation: res.content });
-            rl.question("Do you want to translate another text? Y/n: ", async (answer) => {
-                if (!answer ||answer.toLowerCase() === "y") {
-                    runTranlator();
-                } else {
-                    rl.close();
-                }
-            });
+        const res = await model.invoke(prompt);
+        console.log({ translation: res.content });
+        rl.question("Do you want to translate another text? Y/n: ", async (answer) => {
+            if (!answer ||answer.toLowerCase() === "y") {
+                runTranlator();
+                return
+            } else {
+                rl.close();
+                return
+            }
         });
     });
 }
